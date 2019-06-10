bfaa83819f6f6ea41b27cc16c25804a61c7ea03a
devops-uk <51506761+devops-uk@users.noreply.github.com>
2019-06-10 16:30:53 +0530

    Update first_deployment.tf

--- first_deployment.tf
+++ first_deployment.tf
@@ -1,10 +1,10 @@
 provider "aws" {
-  access_key = "AKIAYMRUE3MNQNIT23ST"
-  secret_key = "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"
+  access_key = "AKIAXMRC7LP6GIBBMB7P"
+  secret_key = "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"
   region     = "us-east-1"
 }
 
-resource "aws_vpc" "web" {
+/*resource "aws_vpc" "web" {
   cidr_block = "198.168.0.0/16"
   instance_tenancy = "default"
   tags = {
@@ -12,6 +12,8 @@ resource "aws_vpc" "web" {
    location = " warangal"
   }
 }
+
+.assign_generated_ipv6_cidr_block
 
 resource "aws_subnet" "subnetwork1"{
   vpc_id     = "${aws_vpc.web.id}"
@@ -22,15 +24,17 @@ resource "aws_subnet" "subnetwork1"{
   }
 
 }
-resource "aws_docdb_cluster" "proj_devops" {
-  cluster_identifier      = "my-docdb-cluster"
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "mydoc2"
   engine                  = "docdb"
-  master_username         = "Irfan"
-  master_password         = "infection"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
   #backup_retention_period = 5
   #preferred_backup_window = "07:00-09:00"
   skip_final_snapshot     = true
 }
+
+
 resource "aws_dynamodb_table" "table1" {
   name = "table1"
   billing_mode   = "PROVISIONED"
@@ -41,7 +45,7 @@ resource "aws_dynamodb_table" "table1" {
     name = "UserId"
     type = "S"
   }
-}
+}*/
 resource "aws_db_instance" "base1" {
   allocated_storage    = 20
   storage_type         = "gp2"
@@ -52,4 +56,15 @@ resource "aws_db_instance" "base1" {
   username             = "foo"
   password             = "foo123456"
   parameter_group_name = "default.mysql5.7"
+}
+
+resource "aws_docdb_cluster" "docdb" {
+  cluster_identifier      = "mydoc2"
+  engine                  = "docdb"
+  master_username         = "foo"
+  master_password         = "mustbeeightchars"
+  #backup_retention_period = 5
+  #preferred_backup_window = "07:00-09:00"
+  skip_final_snapshot     = true
+  snapshot_identifier = "my-docdb-cluster-final-snapshot"
 }
